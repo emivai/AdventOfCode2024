71ca01e089a7234ff8b175c18bd41f9eacac18d5
e-vaisvalaviciute <Emilija.Vaisvalaviciute@adform.com>
2024-12-03 09:31:32 +0200

    day 1

---
+++ AdventOfCode2024/Day1/Day1.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode2024.Day1;
+
+public class Day1
+{
+    private static readonly List<int> LeftList = [];
+    private static readonly List<int> RightList = [];
+
+    public static async Task<long> SolvePart1()
+    {
+        await ReadData();
+        
+        LeftList.Sort();
+        RightList.Sort();
+        
+        return FindDifference();
+    }
+    
+    public static async Task<long> SolvePart2()
+    {
+        await ReadData();
+        
+        LeftList.Sort();
+        RightList.Sort();
+        
+        return FindDifference2();
+    }
+
+    private static async Task ReadData()
+    {
+        var separator = new[] { ' ', '\t' };
+        
+        await foreach (var line in File.ReadLinesAsync(@".\Day1\Day1Input.txt"))
+        {
+            var parts = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            
+            LeftList.Add(int.Parse(parts[0]));
+            RightList.Add(int.Parse(parts[1]));
+        }
+    }
+
+    private static long FindDifference()
+    {
+        long answer = 0;
+
+        for (var i = 0; i < LeftList.Count; i++)
+        {
+            answer += Math.Abs(LeftList[i] - RightList[i]);
+        }
+
+        return answer;
+    }
+    
+    private static long FindDifference2()
+    {
+        long answer = 0;
+
+        for (var i = 0; i < LeftList.Count; i++)
+        {
+            var leftElement = LeftList[i];
+            var rightElement = RightList.Count(j => j == leftElement);
+            
+            if (rightElement != 0)
+                answer += leftElement + rightElement;
+
+        }
+
+        return answer;
+    }
+}
